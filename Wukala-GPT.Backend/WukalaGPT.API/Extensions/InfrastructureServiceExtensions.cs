1de59c45964bb192c4c377167e773aa89213f2d0
HamidTech-Ventures <hamidtechventures@gmail.com>
2026-05-14 09:31:28 -0700

    Fix Npgsql missing port parsing and upgrade MizanAI RAM to 4GB

--- Wukala-GPT.Backend/WukalaGPT.API/Extensions/InfrastructureServiceExtensions.cs
+++ Wukala-GPT.Backend/WukalaGPT.API/Extensions/InfrastructureServiceExtensions.cs
@@ -18,7 +18,8 @@ public static class InfrastructureServiceExtensions
         {
             var uri = new Uri(connectionString);
             var userInfo = uri.UserInfo.Split(':');
-            return $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Prefer;Trust Server Certificate=true;";
+            var port = uri.Port > 0 ? uri.Port : 5432;
+            return $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Prefer;Trust Server Certificate=true;";
         }
 
         return connectionString;
